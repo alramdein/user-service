5e9cd8d9c41b4bdae83cd7b963b02c180371b827
alramdein <ramdanialif26@gmail.com>
2022-12-22 14:06:09 +0700

    feature-fix: fix grpc client

--- client/client.go
+++ client/client.go
@@ -10,9 +10,15 @@ import (
 	"github.com/alramdein/user-service/pb"
 )
 
-func FindUserByUsernameAndPassword(ctx context.Context, username string, password string) (user *pb.User, err error) {
+type userClient struct{}
+
+func NewUserClient() pb.UserServiceClient {
+	return &userClient{}
+}
+
+func (u *userClient) FindUserByUsernameAndPassword(ctx context.Context, in *pb.FindUserByUsernameAndPasswordRequest, opts ...grpc.CallOption) (*pb.User, error) {
 	var conn *grpc.ClientConn
-	conn, err = grpc.Dial(config.GRPCPort(), grpc.WithInsecure())
+	conn, err := grpc.Dial(config.GRPCPort(), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
 		return nil, err
@@ -21,9 +27,9 @@ func FindUserByUsernameAndPassword(ctx context.Context, username string, passwor
 
 	c := pb.NewUserServiceClient(conn)
 
-	user, err = c.FindUserByUsernameAndPassword(ctx, &pb.FindUserByUsernameAndPasswordRequest{
-		Username: username,
-		Password: password,
+	user, err := c.FindUserByUsernameAndPassword(ctx, &pb.FindUserByUsernameAndPasswordRequest{
+		Username: in.Username,
+		Password: in.Password,
 	})
 	if err != nil {
 		log.Fatalf("Error when calling SayHello: %s", err)
