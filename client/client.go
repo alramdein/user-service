39a967e79d94262b9c76c2b537be851a34cd8d8c
alramdein <ramdanialif26@gmail.com>
2022-12-22 13:19:45 +0700

    feature-fix: fix grpc client return statements

--- client/client.go
+++ client/client.go
@@ -10,26 +10,26 @@ import (
 	"github.com/alramdein/user-service/pb"
 )
 
-func FindUserByUsernameAndPassword(ctx context.Context, username string, password string) error {
+func FindUserByUsernameAndPassword(ctx context.Context, username string, password string) (user *pb.User, err error) {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(config.GRPCPort(), grpc.WithInsecure())
+	conn, err = grpc.Dial(config.GRPCPort(), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %s", err)
-		return err
+		return nil, err
 	}
 	defer conn.Close()
 
 	c := pb.NewUserServiceClient(conn)
 
-	response, err := c.FindUserByUsernameAndPassword(ctx, &pb.FindUserByUsernameAndPasswordRequest{
+	user, err = c.FindUserByUsernameAndPassword(ctx, &pb.FindUserByUsernameAndPasswordRequest{
 		Username: username,
 		Password: password,
 	})
 	if err != nil {
 		log.Fatalf("Error when calling SayHello: %s", err)
-		return err
+		return nil, err
 	}
 
-	log.Printf("Response from server: %s", response)
-	return nil
+	log.Printf("Response from server: %s", user)
+	return user, nil
 }
